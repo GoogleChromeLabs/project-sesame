e8dc14906341e3c41740f1ba3c212ada7329dfaa
Eiji Kitamura <agektmr@google.com>
2024-01-06 20:02:09 +0900

    Fix sass import
    Change-Id: I1540bd5436bba1ddcece6eb2ce43b4e04061b6bf

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License
  */
-import sass from 'sass';
+import * as sass from 'sass';
 
 export default {
   entry: {
