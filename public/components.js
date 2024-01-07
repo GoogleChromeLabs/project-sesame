5bb35d1ae28fbcb09517b81c0546056f9fdee93c
Eiji Kitamura <agektmr@google.com>
2024-01-07 23:00:14 +0900

    Apply the Shrine theme
    Change-Id: I65b40cbfaa895c1ae0545649128dc12455003555

--- public/components.js
+++ public/components.js
@@ -14,10 +14,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License
  */
-import '@material/mwc-button';
-import '@material/mwc-top-app-bar-fixed';
+import '@material/mwc-top-app-bar';
+import '@material/mwc-drawer';
 import '@material/mwc-textfield';
-import '@material/mwc-list';
-import '@material/mwc-icon';
-import '@material/mwc-icon-button';
 import '@material/mwc-linear-progress';
+import '@material/web/button/filled-button.js';
+import '@material/web/list/list.js';
+import '@material/web/list/list-item.js';
+import '@material/web/divider/divider.js';
+import '@material/web/icon/icon.js';
+import '@material/web/iconbutton/icon-button.js';
+import '@material/web/progress/linear-progress.js';
