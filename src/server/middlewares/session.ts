ea2e28faad5cbdcebdb84091579d9891a7aacd28
Eiji Kitamura <agektmr@google.com>
2024-03-24 17:18:02 +0900

    Fix
    Change-Id: I5de085c84063a811e68be4ec6ae329f18a704bd4

--- src/server/middlewares/session.ts
+++ src/server/middlewares/session.ts
@@ -249,7 +249,7 @@ export function signOut(req: Request, res: Response) {
   return res.redirect(307, entrancePath);
 }
 
-export function setEntrancePath(req: Request, res: Response, path: string) {
+export function setEntrancePath(req: Request, res: Response, path?: string) {
   req.session.entrance = path || req.path;
   return;
 }
